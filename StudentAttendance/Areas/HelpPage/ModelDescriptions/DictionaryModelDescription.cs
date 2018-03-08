4a20a7b3f3d5bb2b2067ba079e0d8a2e95d14b63
ppowell <ppowell@G0030-03>
2018-03-08 10:33:54 +0000

    Add project files.

---
+++ StudentAttendance/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace StudentAttendance.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ StudentAttendance/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StudentAttendance.WebApiApplication" Language="C#" %>
